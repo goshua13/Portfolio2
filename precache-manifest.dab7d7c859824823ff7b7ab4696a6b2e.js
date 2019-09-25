8ef7a1c383b44d22c02bc90b67cab707b4e3489d
Joshua <joshuaaragon11@gmail.com>
2019-09-25 15:14:17 -0600

    Updates

---
+++ precache-manifest.dab7d7c859824823ff7b7ab4696a6b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5835380363cfd10c05b4",
+    "url": "/Portfolio2/static/css/main.5e54bb63.chunk.css"
+  },
+  {
+    "revision": "5835380363cfd10c05b4",
+    "url": "/Portfolio2/static/js/main.58353803.chunk.js"
+  },
+  {
+    "revision": "ecf6da420028d465a111",
+    "url": "/Portfolio2/static/js/1.ecf6da42.chunk.js"
+  },
+  {
+    "revision": "cb8cecb81e23c0318db8",
+    "url": "/Portfolio2/static/js/runtime~main.cb8cecb8.js"
+  },
+  {
+    "revision": "68d74807e4b97d612e76e1b31bd93fd8",
+    "url": "/Portfolio2/index.html"
+  }
+];
