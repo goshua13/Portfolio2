96c6880e37fd91ad6dfac36d7a511928ff0b2d9f
Joshua Aragon <joshuaaragon11@gmail.com>
2019-09-30 13:29:29 -0600

    Update about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -34,40 +34,31 @@ const About = () => {
 
         <div className="paragraphs">
           <p className="bounce">
-            Hi, I'm a Web developer and I am a Front-end developer who uses
-            HTML, CSS and Several Javascript libraries to create sexy websites. 
+            Hi, I'm a Web developer and I am a Front-end developer who uses 
+            HTML, CSS and Several Javascript libraries to create modern websites. 
             I also use React-Native to build hybrid applications.
           </p>
           <p className="bounce">
-            In high-school I acquired my passion for web and app development
+            In High-school I acquired my passion for Web and App development
             in a 9 week bootcamp through Bottega.
           </p>
           <p className="bounce">
-            2016 - I did code to success which was a bootcamp for web
-            development. I was with a class of 30 students and we worked
-            together on projects to complete a different projects.
+            2016 - I did code to success which was a bootcamp for web development. I was with a class of over 30 students and we worked together to complete a different projects.
           </p>
           <p className="bounce">
-            2016 - 2018: I served in Colombia for two years helping the people
-            better their lives while serving in many leadership and
-            administrative positions
+          2016 - 2018: I was in Colombia for two years serving the people there. I also served in many leadership and administrative positions.
           </p>
           <p className="bounce">
-            2018 - I worked for a Marketing Agency called Imagine Tech. and helped their web development 
-            team create custom websites for their clients.
+            2018 - I worked for a Marketing Agency called Imagine Tech. and helped their web development team create custom websites for their clients.
           </p>
           <p className="bounce">
-            2019 - (May 2019) - I worked as an Intern at BlueRnd working in Reactjs and created websites using Redux. I 
-            also made protoype applications with Cordova that allowed us to use the same code base for 
-            Web, IOS, and Android. 
+        2019 - I worked as an Intern at BlueRnd and worked on several applications from scratch. We used a lot of React with Redux and Cordova to make applications that allowed us to have the same code base for IOS, Android, and Web.
           </p>
           <p className="bounce">
-            2019 (Present) - I Worked as a Front-end developer at UVU leading front-end projects with clients and performing QA 
-            on products with userbases measured in the thousands.
+           2019 (Present) - I Worked as a Front-end developer at UVU leading front-end projects with clients and performing QA on products with userbases measured in the thousands.
           </p>
           <p className="bounce">
-            Extra - I am a skilled communicator with fellow developers and non-technical clients alike. I 
-            live in Utah and, I am fluent in Spanish and intermediate in French!
+            Extra - I am a skilled communicator with fellow developers and non-technical clients alike. I live in Utah and I am fluent in Spanish and intermediate in French.
           </p>
         </div>
       </main>
